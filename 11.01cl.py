8c068965acea6af68b19bb4e84fb07a89dcfd712
Anastasiia <116728907+nesstesi@users.noreply.github.com>
2023-01-15 20:34:09 +0200

    Add files via upload

---
+++ 11.01cl.py
@@ -0,0 +1,6 @@
+import requests
+params = {'latlng': '49.589938,34.550938', 'key': ''}
+response = requests.get('https://maps.googleapis.com/maps/api/geocode', params=params)
+
+print(response)
+print(response.json())
