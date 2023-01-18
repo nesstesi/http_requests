76f30f60ea7367d7e66a06b70fb90993292138c5
Anastasiia <116728907+nesstesi@users.noreply.github.com>
2023-01-18 17:43:28 +0200

    Update 11.01cl.py
    changed url to the right one

--- 11.01cl.py
+++ 11.01cl.py
@@ -1,6 +1,6 @@
 import requests
 params = {'latlng': '49.589938,34.550938', 'key': ''}
-response = requests.get('https://maps.googleapis.com/maps/api/geocode', params=params)
+response = requests.get('https://maps.googleapis.com/maps/api/geocode/json', params=params)
 
 print(response)
 print(response.json())
